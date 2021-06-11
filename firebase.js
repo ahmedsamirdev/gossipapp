43f2e7faf7b6bf7fd2216b3e7bb62be9a3a8ced1
unknown <ahmedsamir551@gmail.com>
2021-06-11 17:08:43 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBfJ81INM-w97aQm15JEnx8CIYgw28-tM",
+  authDomain: "whatsapp-49c16.firebaseapp.com",
+  projectId: "whatsapp-49c16",
+  storageBucket: "whatsapp-49c16.appspot.com",
+  messagingSenderId: "32337511231",
+  appId: "1:32337511231:web:07f6c33da15bd91ab9898a",
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
