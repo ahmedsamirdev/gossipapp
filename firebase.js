fd4b4c1e389edf6d660346678d322fddf478441a
unknown <ahmedsamir551@gmail.com>
2021-06-12 23:14:09 +0200

    edit firebase

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
+  apiKey: "AIzaSyCBfJ81INM-w97aQm15JEnx8CIYgw28-tM",
+  authDomain: "whatsapp-49c16.firebaseapp.com",
+  projectId: "whatsapp-49c16",
+  storageBucket: "whatsapp-49c16.appspot.com",
+  messagingSenderId: "32337511231",
+  appId: "1:32337511231:web:07f6c33da15bd91ab9898a",
 };
 
 const app = !firebase.apps.length
