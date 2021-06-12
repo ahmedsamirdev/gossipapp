8853fcbc8184ca89bb112649ba96f4b919494871
unknown <ahmedsamir551@gmail.com>
2021-06-12 23:08:41 +0200

    env

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+APIKEY:"AIzaSyCBfJ81INM-w97aQm15JEnx8CIYgw28-tM",
+AUTHDOMAIN="whatsapp-49c16.firebaseapp.com",
+PROJECTID="whatsapp-49c16",
+STORAGEBUCKET="whatsapp-49c16.appspot.com",
+MESSAGINGSENDERID="32337511231",
+APPID="1:32337511231:web:07f6c33da15bd91ab9898a",
+    },
+  }
